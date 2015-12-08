1352f5988c2e2d14be8e4ab920e26311254a0f2e
Michael Arnold <valestrum@hotmail.com>
2015-12-08 14:08:45 -0500

    Updated to v0.2.0 thanks to @tapatiolookalikeguy
    tapatiolookalikeguy implemented the OAuth2 PRAW login system.

--- News_Scraper.py
+++ News_Scraper.py
@@ -2,14 +2,15 @@
 import praw         # Python Reddit API Wrapper
 import requests     # ConnectionErrors, rerunning the program.
 import time         # Timer for running the bot every set amount of time
+import OAuth2Util   # Allows for easier handling of OAuth2 with PRAW.
+
 
 r = praw.Reddit(
-    user_agent='Reddit /r/news Scraper - version 0.0.1'
+    user_agent='Reddit /r/news Scraper - version 0.2.0'
                'Created by the #redditbot Reddit Slack community'
                'Designed to scrape /r/news to help identify trending news.'
                 )
-username, password = 'News_Scraper', '#redditbot'
-r.login(username, password)
+o = OAuth2Util.OAuth2Util(r, server_mode=True)
 subreddit = r.get_subreddit("news")
 loop_count = 0
 
